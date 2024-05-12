9c8beb17115441bdd8d90642aedfc8bdde29546e
Messyas <messyasgoisfranca@gmail.com>
2024-05-12 17:48:29 -0400

    Rotas

--- index.ts
+++ index.ts
@@ -1,11 +1,41 @@
+import { Database } from 'bun:sqlite';
+
+const db = new Database('mydb.sqlite', { create: true });
+
+db.run("CREATE TABLE IF NOT EXISTS users (id INTERGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)");
+
+interface User{
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
+
 const server = Bun.serve({
     port: 3000,
-    fetch(req: Request) {
+    async fetch(req: Request) {
         const url = new URL(req.url);
 
         if(url.pathname === '/users') {
 
+            if(req.method === 'POST') {
+
+                const body = await req.json();
+                
+                db.run('INSERT INTO users (name, email, password) values (?,?,?)', body.name , body.email, )
+
+                return new Response(null, { status: 201 });
+
+            } else if(req.method === 'GET') {
+
+                const users: User[] = []
+
+                return Response.json({
+                    users
+                })
+            }
         }
         return new Response('Not found', { status: 404 });
+
     },
 });
